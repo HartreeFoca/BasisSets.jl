1c3e3aa76b785a916d3bfceb265de4bc0144fd08
Leticia-maria <leticiapequeno30@gmail.com>
2023-08-02 14:41:05 -0400

    Fix syntax :shame:

--- test/basis.jl
+++ test/basis.jl
@@ -1,3 +1,4 @@
 @time @testset "basis.jl" begin
     sto3gBasis = parsebasis("./data/water/water.xyz", "sto-3g")
     @test sto3gBasis[1].exponents = round.([130.7093214 23.80886605 6.443608313], digits=4)
+end
