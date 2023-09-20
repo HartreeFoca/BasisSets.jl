78743db0525278469ddc6571adf207d97a041a1c
Leticia-maria <leticiapequeno30@gmail.com>
2023-09-20 09:59:32 -0400

    Make it compatible with ```molecule```

--- src/BasisSets.jl
+++ src/BasisSets.jl
@@ -3,10 +3,13 @@ module BasisSets
     using JSON3
 
     include("periodictable.jl")
+    include("molecule.jl")
     include("parser.jl")
 
     export Atom
     export getatom
+    export Molecule
+    export molecule
     export getatoms
     export parsebasis
 end
