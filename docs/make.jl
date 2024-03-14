482546d958d114265b54ff1a4c0f795afea3417d
Leticia-maria <leticiapequeno30@gmail.com>
2024-03-14 17:42:47 -0400

    Going back to 0.1.1

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,5 @@ makedocs(
 deploydocs(;
     repo="github.com/HartreeFoca/BasisSets.jl.git",
     devbranch="main",
-    branch="main",
-    deploy_config = auto_detect_deploy_system()
+    branch="main"
 )
